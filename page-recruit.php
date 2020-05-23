466e5fdd135fbf41aa9dd3117f6b4660114feb44
KobayashiDaigo <goggggorilla@gmail.com>
2020-05-24 02:15:57 +0900

    add new file

---
+++ page-recruit.php
@@ -0,0 +1,87 @@
+<?php get_header(); ?>
+<main id="recruit">
+    <img src="<?php bloginfo('template_directory'); ?>/images/header3.png" alt="ヘッダーの写真">
+
+    <div class="wrap">
+        <div class="ttl">
+            <p>こどもしぜんじゅくでは</p>
+            <p>一緒に働く仲間を募集しています！</p>
+            <br>
+            <p>個性的なメンバーで、</p>
+            <p>子どもたちの日々の成長に感動しながら働いております。</p>
+            <p>こどもが大好きで、明るく元気な方の応募お待ちしております。</p>
+        </div>
+    </div>
+
+    <div class="recruit">
+        <div class="wrap">
+            <ul class="recruit-ttl">
+                <li class="active">正社員(中途/新卒)</li>
+                <li>パート/アルバイト</li>
+            </ul>
+            <table class="recruit-content show">
+                <tr>
+                    <td>職種</td>
+                    <td>こどもしぜんじゅく講師</td>
+                </tr>
+                <tr>
+                    <td>雇用形態</td>
+                    <td>正社員</td>
+                </tr>
+                <tr>
+                    <td>仕事内容</td>
+                    <td>子どもたちが自然に興味を持つような講義を実施</td>
+                </tr>
+                <tr>
+                    <td>給与</td>
+                    <td>200,000万円(能力、評価による)</td>
+                </tr>
+                <tr>
+                    <td>勤務時間</td>
+                    <td>7:30～16:30(休憩60分)</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>月～日曜の週5日勤務(シフト制)</td>
+                </tr>
+                <tr>
+                    <td>勤務地</td>
+                    <td>北海道旭川市自然町塾</td>
+                </tr>
+            </table>
+            <table class="recruit-content">
+                <tr>
+                    <td>職種</td>
+                    <td>こどもしぜんじゅく講師</td>
+                </tr>
+                <tr>
+                    <td>雇用形態</td>
+                    <td>パート/アルバイト(フルタイム)</td>
+                </tr>
+                <tr>
+                    <td>仕事内容</td>
+                    <td>子どもたちが自然に興味を持つような講義を実施</td>
+                </tr>
+                <tr>
+                    <td>給与</td>
+                    <td>時給1,000円(昇給有)</td>
+                </tr>
+                <tr>
+                    <td>勤務時間</td>
+                    <td>7:30～16:30(休憩60分)</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>月～日曜の週3～5日勤務(シフト制)</td>
+                </tr>
+                <tr>
+                    <td>勤務地</td>
+                    <td>北海道旭川市自然町塾</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+
+</main>
+
+<?php get_footer(); ?>
