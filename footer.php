1cf7bbc0cf130e8b6cb34d3e97ab5707834ff6ab
KobayashiDaigo <goggggorilla@gmail.com>
2020-05-24 02:24:50 +0900

    add new file

--- footer.php
+++ footer.php
@@ -3,36 +3,15 @@
         <?php echo do_shortcode('[contact-form-7 id="168" title="NSCform"]'); ?>
     </div>
 
-    <div class="article">
-        <div class="wrap">
-        <p>お知らせ</p>
-            <?php 
-            if(have_posts()):
-                while(have_posts()):the_post();
-            ?>
-            <div class="article-text">
-                <a href="<?php echo get_permalink(); ?>">
-                    <?php the_time('Y.n.j'); ?>&nbsp;&nbsp;
-                </a>
-                <a href="<?php echo get_permalink(); ?>">
-                    <?php the_category(', ') ?>&nbsp;&nbsp;
-                </a>
-                <a href="<?php echo get_permalink(); ?>">
-                    <?php the_title(); ?>&nbsp;&nbsp;
-                </a>
-            </div>
-            <?php 
-            endwhile;
-            endif;
-            ?>
-        </div>
-    </div>
 
 
     <div id="access">
         <div class="wrap">
             <p>アクセス</p>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2881.214295487939!2d142.47784931481536!3d43.76841027911746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f0ce0ccb5e023cb%3A0xc825dcbd1da853d3!2z5pet5bed5biC5pet5bGx5YuV54mp5ZyS!5e0!3m2!1sja!2sjp!4v1590025147628!5m2!1sja!2sjp" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <div class="gmap">
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2881.214295487939!2d142.47784931481536!3d43.76841027911746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f0ce0ccb5e023cb%3A0xc825dcbd1da853d3!2z5pet5bed5biC5pet5bGx5YuV54mp5ZyS!5e0!3m2!1sja!2sjp!4v1590025147628!5m2!1sja!2sjp" width="100%" height="auto" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
             <p>こどもしぜんじゅく</p>
             <p>〒123-4567</p>
             <p>北海道旭川市子供町自然塾</p>
@@ -41,7 +20,7 @@
 
     <div class="nospace">
         <div class="recruit">
-            <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/recruit1.png" alt="採用情報"></a>
+            <a href="<?php echo home_url('/recruit'); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/recruit1.png" alt="採用情報"></a>
         </div>
     </div>
     
@@ -51,8 +30,8 @@
 
     <div class="footer">
         <div>
-            <a href="">お知らせ</a>
-            <a href="">採用情報</a>
+            <a href="<?php echo home_url('/news'); ?>">お知らせ</a>
+            <a href="<?php echo home_url('/recruit'); ?>">採用情報</a>
             <div>
                 <a href="#" class="insta_btn"><span class="insta"><i class="fab fa-instagram"></i></a>
                 <a href="#" class="twitter_btn"><span class="twitter"><i class="fab fa-twitter"></i></a>
